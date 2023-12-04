7b3146b45b896c32b1d024a25c8311f15b796001
Macauley Morgan <macauley.main@gmail.com>
2023-12-04 18:26:29 +0000

    Changed about page to recipes page

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
 
 
 @app.route("/contact")
