098f549acac03b23ee2f36dd7eb3b476a8386a35
Macauley Morgan <macauley.main@gmail.com>
2023-12-09 11:09:49 +0000

    Connected HTML sign in form to flask

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",   methods =['GET', 'POST'])
 def landing():
+    if request.method == 'POST':
+        email = request.form.get("sign-in-email-address")
+        password = request.form.get("password")
+        user = [email, password]
+        print(user)
     return render_template("landing.html")
 
 
@@ -40,7 +45,7 @@ def account():
     return render_template("account.html")
 
 
-@app.route("/landing")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
